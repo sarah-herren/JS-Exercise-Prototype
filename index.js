a1d4f28efd50022f5fec2b0ea60b4524a75e6218
Sarah Herren <sarah@Sarahs-Air.attlocal.net>
2020-12-08 19:36:37 -0600

    MVP

--- index.js
+++ index.js
@@ -39,15 +39,25 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
-  }
- 
- 
+ function Person(name, age) {
+    this.name = name,
+    this.age = age,
+    this.stomach = []
+  };
 
-  
-  
-  
+  Person.prototype.eat = function (edible){
+    if(this.stomach.length < 10){
+      this.stomach.push(edible);
+    }
+  };
+
+  Person.prototype.poop = function(){
+    this.stomach = [];
+  };
+
+  Person.prototype.toString = function(){
+    return `${this.name}, ${this.age}`
+  };
   
   /*
     TASK 2
@@ -63,10 +73,16 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
   }
   
+  Car.prototype.fill = function(gallons){
+    return this.tank += gallons
+  };
   
   /*
     TASK 3
@@ -75,18 +91,25 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
-  }
+ function Baby(name, age, favoriteToy) {
+   Person.call(this, name, age);
+   this.favoriteToy = favoriteToy;
+  };
+
+  Baby.prototype = Object.create(Person.prototype);
+
+  Baby.prototype.play = function(){
+    return `Playing with ${this.favoriteToy}`;
+  };
  
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. Most common ussage is implicit binding - look to the left of the dot and that is what this references!
+    2. explicit binding is using the .call, .bind commands to tell JS what this is bound to
+    3. the new keyword creates a new object via the constructor function - this points to the new object.
+    4. if none of the the above rules apply this bnds to the window
   */
   
   
